05c0e14e14d47a94b558ffaefc07b87a08af06eb
VladPashkevich <vladpashkevich1993@gmail.com>
2022-06-15 23:30:52 +0300

    bloggersapi2lesson fix idval

--- src/middlewares/idValidator.ts
+++ src/middlewares/idValidator.ts
@@ -1,6 +1,9 @@
 import { param } from 'express-validator';
 
-export const idValidator = param('id')
+export const idValidator = param(['id', 'bloggerId', 'postId'])
   .exists()
+  .withMessage('Value should be exists')
+  .notEmpty()
+  .withMessage('Value should be not empty')
   .isInt({ min: 1 })
   .withMessage('Value should be number');
