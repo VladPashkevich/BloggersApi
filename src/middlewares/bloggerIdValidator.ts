1ebdb065debf8b06466ef47ef9f226a8d053a57c
VladPashkevich <vladpashkevich1993@gmail.com>
2022-06-16 01:01:31 +0300

    bloggersapi2lesson fix validatidblog

--- src/middlewares/bloggerIdValidator.ts
+++ src/middlewares/bloggerIdValidator.ts
@@ -1,7 +1,15 @@
 import { body } from 'express-validator';
+import { bloggers } from '../repositories/database';
 
 export const bodyBloggerIDValidator = body('bloggerId')
   .trim()
   .notEmpty()
   .isInt({ min: 1, max: 2147483647 })
-  .withMessage('Value should be number');
+  .withMessage('Value should be number')
+  .custom((bloggerId) => {
+    const blogger = bloggers.find((blogger) => blogger.id == bloggerId);
+    if (!blogger) {
+      throw new Error('BloggerId does not exists');
+    }
+    return true;
+  });
