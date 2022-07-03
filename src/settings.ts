34157dc6b2b3241268050e2adddead0abf911d44
VladPashkevich <vladpashkevich1993@gmail.com>
2022-07-03 22:51:42 +0300

    bloggersapi4lesson

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,3 @@
-import 'dotenv/config';
-
 export const settings = {
   MONGO_URI: process.env.mongoUri || 'mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || '123',
