fd30937f735c3f456dd7571e806d5c9d924fdae1
VladPashkevich <vladpashkevich1993@gmail.com>
2022-07-05 14:23:39 +0300

    bloggersapi4lesson authbasic

--- src/middlewares/basicAutht.ts
+++ src/middlewares/basicAutht.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const superAdminAuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-  const base64 = Buffer.from('admin:qwerty').toString('base64');
-  const encode = `Basic ${base64}`;
+  const authHeader = req.headers?.authorization;
+  //const base64 = Buffer.from('admin:qwerty').toString('base64');
+  const encode = `Basic YWRtaW46cXdlcnR5`;
   if (authHeader === encode) {
     next();
   } else {
