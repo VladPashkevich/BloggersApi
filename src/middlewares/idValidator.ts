f9e7bc8c6653e6993fa14c94d4850581eb7b739d
VladPashkevich <vladpashkevich1993@gmail.com>
2022-07-05 16:52:15 +0300

    bloggersapi4lesson fix id in mongo

--- src/middlewares/idValidator.ts
+++ src/middlewares/idValidator.ts
@@ -7,14 +7,21 @@ export const mongoIdValidator =
     const id = req.params?.[param];
     console.log(req.params);
     console.log(id);
-    try {
-      new ObjectId(id);
+    if (isValidObjectID(id)) {
       next();
-    } catch (e) {
+    } else {
       res.send(404);
     }
   };
-
+function isValidObjectID(str: string) {
+  str = str + '';
+  var len = str.length,
+    valid = false;
+  if (len == 12 || len == 24) {
+    valid = /^[0-9a-fA-F]+$/.test(str);
+  }
+  return valid;
+}
 export const paramBloggerIDValidator = param('bloggerId').isMongoId();
 
 export const paramPostIDValidator = param('postId').isMongoId();
